ad7f084cc69797920703f518629b8d416926b06f
FARAI Chakarisa <faraichaka@gmail.com>
2022-10-23 15:13:08 +0200

    update

--- producer/Controllers/HomeController.cs
+++ producer/Controllers/HomeController.cs
@@ -70,10 +70,14 @@ namespace producer.Controllers
 
                 const string url = "https://imspulse.com/bunch-box/HJ/Paper.html";
                 // Set page size A3 and Landscape orientation;   
-                Aspose.Words.HtmlLoadOptions options = new Aspose.Words.HtmlLoadOptions();
-                
-                Aspose.Words.Document pdfDocument = new Aspose.Words.Document(GetContentFromUrlAsStream(url), options);
-                pdfDocument.Save(fullpath + "/" + "html_test.docx", Aspose.Words.SaveFormat.Docx);
+                HtmlLoadOptions options = new HtmlLoadOptions(url)
+                {
+                    PageInfo = { Width = 840, Height = 1188, IsLandscape = false },
+
+                   
+                };
+                Document pdfDocument = new Document(GetContentFromUrlAsStream(url), options);
+                pdfDocument.Save(fullpath + "/" + "html_test.docx", SaveFormat.DocX);
                 return new JsonResult("Saved!");
             }
             catch (Exception ex)
