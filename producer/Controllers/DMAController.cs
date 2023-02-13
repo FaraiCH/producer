38ad2c7d0628045897d932d1ed4793d8b382a013
FARAI Chakarisa <faraichaka@gmail.com>
2023-02-13 19:29:53 +0200

    upddate

---
+++ producer/Controllers/DMAController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DMAController : ControllerBase
+    {
+        // GET: api/<DMAController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DMAController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DMAController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DMAController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DMAController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- producer/Controllers/HomeController.cs
+++ producer/Controllers/HomeController.cs
@@ -53,47 +53,29 @@ namespace producer.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult PDFView(string fullName)
+        public IActionResult PDFConvert(string fullName)
         {
             Exec("sudo chmod 775 -R /var/www/html/imspulse/bunch-box");
             string LData = "PExpY2Vuc2U+CjxEYXRhPgo8TGljZW5zZWRUbz5BdmVQb2ludDwvTGljZW5zZWRUbz4KPEVtYWlsVG8+aXRfYmlsbGluZ0BhdmVwb2ludC5jb208L0VtYWlsVG8+CjxMaWNlbnNlVHlwZT5EZXZlbG9wZXIgT0VNPC9MaWNlbnNlVHlwZT4KPExpY2Vuc2VOb3RlPkxpbWl0ZWQgdG8gMSBkZXZlbG9wZXIsIHVubGltaXRlZCBwaHlzaWNhbCBsb2NhdGlvbnM8L0xpY2Vuc2VOb3RlPgo8T3JkZXJJRD4xOTA1MjAwNzE1NDY8L09yZGVySUQ+CjxVc2VySUQ+MTU0ODI2PC9Vc2VySUQ+CjxPRU0+VGhpcyBpcyBhIHJlZGlzdHJpYnV0YWJsZSBsaWNlbnNlPC9PRU0+CjxQcm9kdWN0cz4KPFByb2R1Y3Q+QXNwb3NlLlRvdGFsIGZvciAuTkVUPC9Qcm9kdWN0Pgo8L1Byb2R1Y3RzPgo8RWRpdGlvblR5cGU+RW50ZXJwcmlzZTwvRWRpdGlvblR5cGU+CjxTZXJpYWxOdW1iZXI+Y2JmMzVkNWYtOWE2Ni00ZTI4LTg1ZGQtM2ExN2JiZTM0MTNhPC9TZXJpYWxOdW1iZXI+CjxTdWJzY3JpcHRpb25FeHBpcnk+MjAyMDA2MDQ8L1N1YnNjcmlwdGlvbkV4cGlyeT4KPExpY2Vuc2VWZXJzaW9uPjMuMDwvTGljZW5zZVZlcnNpb24+CjxMaWNlbnNlSW5zdHJ1Y3Rpb25zPmh0dHBzOi8vcHVyY2hhc2UuYXNwb3NlLmNvbS9wb2xpY2llcy91c2UtbGljZW5zZTwvTGljZW5zZUluc3RydWN0aW9ucz4KPC9EYXRhPgo8U2lnbmF0dXJlPnpqZDMrdWgzNTdiZHhqR3JWTTZCN3I2c250TkRBTlRXU2MyQi9RWS9hdmZxTnA0VHk5Z0kxR2V1NUdOaWVwRHArY1JrRFBMdjBDRTZ2MHNjYVZwK1JNTkF5SzdiUzdzeGZSL205Z0NtekFNUlptdUxQTm1laEtZVTNvOGJWVDJvWmRJeEY2dVRTMDhIclJxUnk5SWt6c3BxYmRrcEZFY0lGcHlLbDF2NlF2UT08L1NpZ25hdHVyZT4KPC9MaWNlbnNlPg==";
 
             Stream stream4 = new MemoryStream(Convert.FromBase64String(LData));
-            Stream stream2 = new MemoryStream(Convert.FromBase64String(LData));
+
             new Aspose.Pdf.License().SetLicense(stream4);
-            new Aspose.Words.License().SetLicense(stream2);
             try
             {
-                Document document = new Document(fullName);
-                string fullpath = Path.GetDirectoryName(fullName);     
-                document.Save(fullpath + "/" + "Paper.html", SaveFormat.Html);
-
-                const string url = "https://imspulse.com/bunch-box/HJ/Paper.html";
-                // Set page size A3 and Landscape orientation;   
-                HtmlLoadOptions options = new HtmlLoadOptions(url)
+                Document pdfDocument = new Document(fullName);
+                DocSaveOptions saveOptions = new DocSaveOptions
                 {
-                    PageInfo = { Width = 840, Height = 1188, IsLandscape = false },
-
-                   
+                    Format = DocSaveOptions.DocFormat.DocX,
+                    // Set the recognition mode as Flow
+                    Mode = DocSaveOptions.RecognitionMode.Flow,
+                    // Set the Horizontal proximity as 2.5
+                    RelativeHorizontalProximity = 2.5f,
+                    // Enable the value to recognize bullets during conversion process
+                    RecognizeBullets = true
                 };
-                Document pdfDocument = new Document(GetContentFromUrlAsStream(url), options);
-                //pdfDocument.OptimizeResources(new Document.OptimizationOptions()
-
-                //{
-
-                //    LinkDuplcateStreams = true,
-
-                //    RemoveUnusedObjects = true,
-
-                //    RemoveUnusedStreams = true,
-
-                //    CompressImages = true,
-
-                //    ImageQuality = 70
-
-                //});
-                //pdfDocument.Save(fullpath + "/" + "html_test.docx", SaveFormat.DocX);
-                return new JsonResult(GetContentFromUrlAsStream(url));
+                pdfDocument.Save("/var/www/html/imspulse/bunch-box/tata.docx", saveOptions);
+                return new JsonResult("Save");
             }
             catch (Exception ex)
             {
