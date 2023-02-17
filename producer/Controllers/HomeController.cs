d156a4afde89d6802f9d426c89464552e305f44a
Farai Chakarisa <faraichaka@gmail.com>
2023-02-17 10:43:49 +0200

    update

--- producer/Controllers/HomeController.cs
+++ producer/Controllers/HomeController.cs
@@ -69,7 +69,7 @@ namespace producer.Controllers
                 {
                     Format = DocSaveOptions.DocFormat.Doc,
                     // Set the recognition mode as Flow
-                    Mode = DocSaveOptions.RecognitionMode.Textbox,
+                    Mode = DocSaveOptions.RecognitionMode.Flow,
                     // Set the Horizontal proximity as 2.5
                     RelativeHorizontalProximity = 2.5f,
                     // Enable the value to recognize bullets during conversion process
